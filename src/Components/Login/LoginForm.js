cf8fa90f23d89a67a2993eba67c56d1c01c85c7d
Giancarlo Horta <giancarlohorta@gmail.com>
2020-08-19 17:19:02 -0300

    finish

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -32,7 +32,7 @@ const LoginForm = () => {
         ) : (
           <Button>Entrar</Button>
         )}
-        <Error error={error} />
+        <Error error={error && "Dados incorretos"} />
       </form>
       <Link className={styles.lost} to="/login/lost">
         Perdeu a senha?
